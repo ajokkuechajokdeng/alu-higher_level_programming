f1ae106d1da8d5e92e24a66df11b1b272611f5b4
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-02-07 05:34:59 -0800

    My Hello,world

--- python-hello_world/100-write.py
+++ python-hello_world/100-write.py
@@ -1,25 +1,4 @@
-#include "lists.h"
-/**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
- */
-int check_cycle(listint_t *list)
-{
-	listint_t *slow = list;
-	listint_t *fast = list;
-
-	if (!list)
-		return (0);
-
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return (1);
-	}
-
-	return (0);
-}
+#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
