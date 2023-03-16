d2bed28b7d85eba7f3321a4625c1b8778630fd4e
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-03-16 10:06:22 -0700

    Python - Classes and Objects

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,64 @@
+#!/usr/bin/python3
+"""
+This is the Square module.
+This module contains one class: Square.
+"""
+
+
 class Square:
+    """
+    This class defines a square.
+    """
+
     def __init__(self, size=0):
-        """Initialize a new instance of the Square class."""
-        self.__size = size
+        """
+        Initializes an instance of Square.
 
-        if not isinstance(size, int):
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-
-        if size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
+        """
+        Computes the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
+
