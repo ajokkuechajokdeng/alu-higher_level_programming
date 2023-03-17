5630327f44ccfc67119e213aea6a56eb0d370393
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-03-17 11:56:39 -0700

    Python - More Classes and Objects

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -67,5 +67,5 @@ class Rectangle:
                 rectangle += "\n"
         return (rectangle)
 
-    def _repr_(self):
+    def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
