50b3b2eb1d5066504210095c31f1914c81e22f37
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-03-17 11:35:43 -0700

    Python - More Classes and Objects

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+This module contains the Rectangle class
+"""
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return "\n".join(["#" * self.__width] * self.__height)
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
