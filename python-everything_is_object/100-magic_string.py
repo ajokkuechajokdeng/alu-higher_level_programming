4b16b461c2e75d40bf22578cb3b42d996eff18d4
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-03-23 16:40:43 -0700

    Everything is object

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-100- #!/usr/bin/python3
+#!/usr/bin/python3
 def magic_string():
     magic_string.counter = getattr(magic_string, 'counter', 0) + 1
     return ', '.join(['Holberton']*magic_string.counter)
