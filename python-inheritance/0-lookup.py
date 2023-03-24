5f3f6ab2eb183537fb664ffb3452dea0caf91d40
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-03-24 04:27:27 -0700

    Inheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,14 +1,3 @@
-#!/usr/bin/python3
-"""
-Module that defines a lookup function.
-"""
-
-
-def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object.
-    """
-    return [item for item in dir(obj) if not item.startswith('__')]
 class MyClass1(object):
     pass
 
