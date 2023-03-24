9f10ad9f72a7a7a8f1d83c1a3a85348ae046c682
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-03-24 04:25:39 -0700

    Inheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -9,3 +9,14 @@ def lookup(obj):
     Returns the list of available attributes and methods of an object.
     """
     return [item for item in dir(obj) if not item.startswith('__')]
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
