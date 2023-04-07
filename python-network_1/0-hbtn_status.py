f550450e159d9b7479e5501320055bc161a3b180
ajokkuechajokajok <a.deng2@alustudent.com>
2023-04-07 16:53:40 +0000

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""This script fetches https://intranet.hbtn.io/status using the urllib package
-"""
+'''a Python script that fetches a url'''
+
+
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    data = response.read()
-    print("Body response:")
-    print("     - type:", type(data))
-    print("     - content:", data)
-    print("     - utf8 content:", data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
