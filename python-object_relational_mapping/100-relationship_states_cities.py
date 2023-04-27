9db546ea9fc224b172f9eb23d56b4348265a56c3
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-04-27 09:18:28 -0700

    Python - Object-relational mapping

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
+# script that creates the State “California” with the City,
+# “San Francisco” from the database hbtn_0e_100_usa
 """
-Prints the State object with the name passed as argument from the db
+    import 'sys','sqlalchemy' & 'relationship_..'
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import Base, City
 
-
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-    print("Not found" if not state else state.id)
+    session.add(City(name="San Francisco", state=State(name="California")))
+    session.commit()
