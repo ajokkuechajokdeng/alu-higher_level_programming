8205db9a1756d28004904f07e710eb0bce67ded4
ajokkuechajokdeng <a.deng2@alustudent.com>
2023-06-16 12:48:39 -0700

    updated

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.sort(() => 1);
+  const arr = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    arr.push(list[i]);
+  }
+  return (arr);
 };
